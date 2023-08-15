56caf71d9a8887ca6f06b02a1f488e8c9548f4dd
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-08-14 21:51:07 -0400

    builds the Dashboard, Loading and Panel components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,55 @@
 import React, { Component } from "react";
+import Loading from "./Loading";
+import Panel from "./Panel";
 
 import classnames from "classnames";
 
-class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
-
-    return <main className={dashboardClasses} />;
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng"
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: "Lukas Souza"
   }
-}
+];
 
-export default Dashboard;
+export default class Dashboard extends Component {
+
+  state = { loading: false, focused: null };
+
+  toggleFocus(id) { this.setState(prev => ({ focused: prev.focused ? null : id })); };
+
+  render() {
+    const dashboardClasses = classnames("dashboard", { "dashboard--focused": this.state.focused });
+
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(datum => {
+        const { id, label, value } = datum;
+        return <Panel key={id} label={label} value={value} toggleFocus={e => this.toggleFocus(id)} />;
+      });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    return (
+      <main className={dashboardClasses}>
+        {panels}
+      </main>
+    );
+  };
+};
