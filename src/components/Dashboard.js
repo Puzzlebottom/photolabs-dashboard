e5c6c50b92fb9d27c80250891ed85cab210a19ad
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-08-14 22:07:56 -0400

    persists panel focus in local state and adds api call to backend in componentDidMount()

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,34 +2,59 @@ import React, { Component } from "react";
 import Loading from "./Loading";
 import Panel from "./Panel";
 
+import { getTotalPhotos, getTotalTopics, getUserWithMostUploads, getUserWithLeastUploads } from "helpers/selectors";
 import classnames from "classnames";
 
 const data = [
   {
     id: 1,
     label: "Total Photos",
-    value: 10
+    getValue: getTotalPhotos
   },
   {
     id: 2,
     label: "Total Topics",
-    value: 4
+    getValue: getTotalTopics
   },
   {
     id: 3,
     label: "User with the most uploads",
-    value: "Allison Saeng"
+    getValue: getUserWithMostUploads
   },
   {
     id: 4,
     label: "User with the least uploads",
-    value: "Lukas Souza"
+    getValue: getUserWithLeastUploads
   }
 ];
 
 export default class Dashboard extends Component {
 
-  state = { loading: false, focused: null };
+  state = { photos: [], topics: [], loading: true, focused: null };
+
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem('focused'));
+
+    if (focused) this.setState({ focused });
+
+    const urlsPromise = ['/api/photos', '/api/topics']
+      .map(url => fetch(url).then(res => res.json()));
+
+    Promise.all(urlsPromise)
+      .then(([photos, topics]) => {
+        this.setState({
+          loading: false,
+          photos,
+          topics
+        });
+      });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.focused !== this.state.focused) {
+      localStorage.setItem('focused', JSON.stringify(this.state.focused));
+    }
+  }
 
   toggleFocus(id) { this.setState(prev => ({ focused: prev.focused ? null : id })); };
 
@@ -38,8 +63,8 @@ export default class Dashboard extends Component {
 
     const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
       .map(datum => {
-        const { id, label, value } = datum;
-        return <Panel key={id} label={label} value={value} toggleFocus={e => this.toggleFocus(id)} />;
+        const { id, label, getValue } = datum;
+        return <Panel key={id} label={label} value={getValue(this.state)} toggleFocus={e => this.toggleFocus(id)} />;
       });
 
     if (this.state.loading) {
